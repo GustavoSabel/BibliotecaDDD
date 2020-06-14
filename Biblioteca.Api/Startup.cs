6736c1862f07fd29d2fa9f352eeee1d7bb2d5adb
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2020-06-14 13:40:49 -0300

    Usando Swagger

--- Biblioteca.Api/Startup.cs
+++ Biblioteca.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Biblioteca.Api
 {
@@ -36,6 +37,12 @@ namespace Biblioteca.Api
             services.AddDbContext<BibliotecaContext>(options =>
             {
                 BibliotecaContext.Configurar(options, @"Server=.;Database=Biblioteca;Integrated Security=True", WebHostEnvironmen.IsDevelopment());
+            }); 
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API da Biblioteca", Version = "v1" });
             });
         }
 
@@ -45,6 +52,9 @@ namespace Biblioteca.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API da Biblioteca V1"));
 
             app.UseRouting();
 
